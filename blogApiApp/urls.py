32211c4b41ba02ebc4578d3e6f2eb6f8be475c9b
marcostota <juniorvans2@gmail.com>
2023-07-27 15:50:58 -0300

    first commit

---
+++ blogApiApp/urls.py
@@ -0,0 +1,10 @@
+from  django.urls import path
+from . import views
+
+urlpatterns = [
+    path('get-all-posts/', views.get_all_posts),
+    path('create-post/', views.create_post),
+    path('delete-post/', views.delete_post),
+    path('get-post/', views.get_post),
+    path('update-post/', views.update_post),
+]
